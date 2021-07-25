a098d3f3cb08c42562441d2fbf79a6632b0546e6
Zara1234567 <72318811+Zara1234567@users.noreply.github.com>
2021-07-25 15:03:33 +0100

    Add files via upload

---
+++ class 43 project/js/Code.js
@@ -0,0 +1,33 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("U C N T N I F O", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+
+
+    fill("white")
+    textSize(15)
+    text("A T A B D S E A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information!!", 100,270)
+
+
+
+// add code for displaying rest of the names and the hints.
+
+    
+
+}
+
